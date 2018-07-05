de4face9002417031da6001a950194b579c3aee7
Kanwar <nitin.kanwar@uta.edu>
2018-07-05 16:30:40 -0500

    Committing graphs related code

--- learning/part1/chapter3/explore.R
+++ learning/part1/chapter3/explore.R
@@ -16,3 +16,32 @@ colnames(statef) <- c("state","count")
 summary(statef)
 statef <- transform(statef, state=reorder(state,count))
 summary(statef)
+ggplot(statef) + geom_bar(aes(x=state, y=count), fill="gray", stat="identity") + coord_flip() + theme(axis.text.y=element_text(size=rel(0.8)))
+
+
+
+#dummy data 
+
+months <-rep(c("jan", "feb", "mar", "apr", "may", "jun", 
+               "jul", "aug", "sep", "oct", "nov", "dec"), 2)
+chickens <-c(1, 2, 3, 3, 3, 4, 5, 4, 3, 4, 2, 2)
+eggs <-c(0, 8, 10, 13, 16, 20, 25, 20, 18, 16, 10, 8)
+values <-c(chickens, eggs)
+type <-c(rep("chickens", 12), rep("eggs", 12))
+mydata <-data.frame(months, values)
+p <-ggplot(mydata, aes(months, values))
+p +geom_bar(stat = "identity")
+mydata$months <-factor(mydata$months, 
+                       levels = c("jan", "feb", "mar", "apr", "may", "jun",
+                                  "jul", "aug", "sep", "oct", "nov", "dec"))
+
+p <-ggplot(mydata, aes(months, values))
+p + geom_bar(stat = "identity", aes(fill = type))  # this will show two types in the bar chart
+
+p <-ggplot(mydata, aes(months, values))
+p + geom_bar(stat = "identity", aes(fill = type), position = "dodge") # this will show bars stacked next to each other
+  +xlab("Months") + ylab("Count") +
+  ggtitle("Chickens & Eggs") +
+  theme_bw()
+
+
