b3dc0b356f016f2dae410dbce121f45ea982c358
Kanwar <nitin.kanwar@uta.edu>
2018-07-05 17:42:25 -0500

    Committing graphs related code

--- learning/part1/chapter3/explore.R
+++ learning/part1/chapter3/explore.R
@@ -20,8 +20,7 @@ ggplot(statef) + geom_bar(aes(x=state, y=count), fill="gray", stat="identity") +
 
 
 
-#dummy data 
-
+#dummy data and plotting a bar chart
 months <-rep(c("jan", "feb", "mar", "apr", "may", "jun", 
                "jul", "aug", "sep", "oct", "nov", "dec"), 2)
 chickens <-c(1, 2, 3, 3, 3, 4, 5, 4, 3, 4, 2, 2)
@@ -44,4 +43,30 @@ p + geom_bar(stat = "identity", aes(fill = type), position = "dodge") # this wil
   ggtitle("Chickens & Eggs") +
   theme_bw()
 
+# plotting a line plot
+x <- runif(100)
+y <- 2*x^2 + 10
+ggplot(data.frame(x=x,y=y), aes(x=x,y=y)) + geom_line()
 
+
+custdata3 <- subset(custdata,
+                    (custdata$age > 0 & custdata$age < 100
+                     & custdata$income > 0))
+
+ggplot(custdata3, aes(x=age, y=income)) +
+  geom_point() + ylim(0, 200000)
+
+ggplot(custdata3, aes(x=age, y=income)) + geom_point() +
+  stat_smooth(method="lm") +
+  ylim(0, 200000)
+
+ggplot(custdata3, aes(x=age, y=income)) +
+  geom_point() + geom_smooth() +
+  ylim(0, 200000)
+
+ggplot(custdata3, aes(x=age, y=as.numeric(health.ins))) +
+  geom_point(position=position_jitter(w=0.05, h=0.05)) +
+  geom_smooth()
+
+ggplot(custdata) + geom_bar(aes(x=marital.stat,
+                                fill=health.ins))
---
+++ learning/part1/chapter4/chapter4.R
@@ -0,0 +1,18 @@
+load("~/R/learning/part1/chapter4/exampleData.rData")
+
+custdata$is.employed.fix <- ifelse(is.na(custdata$is.employed),"missing", ifelse(custdata$is.employed == T, "employed", "unemployed"))
+
+custdata$is.employed.fix <- ifelse(is.na(custdata$is.employed),"not in active work force", ifelse(custdata$is.employed == T, "employed", "unemployed"))
+
+meanIncome <- mean(custdata$Income, na.rm = TRUE)
+custdata$income.fix <- ifelse(is.na(custdata$Income), meanIncome, custdata$Income)
+summary(custdata$income.fix)
+
+breaks <- c(0,10000, 50000, 100000, 250000, 1000000)
+
+income.groups <- cut(custdata$income, breaks, include.lowest = TRUE)
+income.groups
+income.groups <- as.character(income.groups)
+income.groups <- ifelse(is.na(income.groups),"no income", income.groups)
+summary(income.groups)
+
