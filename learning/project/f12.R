6dd4b69e568a693bf723e0629e2fd1025f5f1841
Kanwar <nitin.kanwar@uta.edu>
2018-07-09 17:01:12 -0500

    Committing faceted graph

--- learning/project/f12.R
+++ learning/project/f12.R
@@ -27,3 +27,15 @@ calcLapTimes <- function(lapTime){
     return(0)
   }
 }
+
+lapTimesresults$lapTimes <- sapply(as.character(results$fastestLapTime), calcLapTimes)
+races$date <- as.Date(races$date)
+races$name<-gsub(" Grand Prix","",races$name)
+results_modified <- merge(results, races, by="raceId")
+lap_times_per_year <- results_modified[c("name","lapTimes","year")]
+aggregate(lap_times_per_year, lap_times_per_year[c("year","name")], mean, na.rm = TRUE)
+avg_lapTimes_per_year_filtered <- subset(avg_lapTimes_per_year[c("name", "lapTimes", "year")], year>=2004)
+
+# Faceted plot to display the average speed over years at specific Grand Prix venues
+ggplot(avg_lapTimes_per_year_filtered) + geom_line(aes(x=avg_lapTimes_per_year_filtered$year, y=avg_lapTimes_per_year_filtered$lapTimes, group=avg_lapTimes_per_year_filtered$name, color = avg_lapTimes_per_year_filtered$name)) + facet_wrap(~avg_lapTimes_per_year_filtered$name) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + xlab("Year") + ylab("Lap time in seconds") + ggtitle("Lap Times over the years per Grand Prix") + guides(color=guide_legend(title="Grand Prix"))
+
